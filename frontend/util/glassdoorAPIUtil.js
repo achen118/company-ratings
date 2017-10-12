a47eb34e47ea0683df7cb51c94f674f2151839fe
Alice Chen <alicechen118@gmail.com>
2017-10-12 01:42:24 -0700

    Fix glassdoor api url

--- frontend/util/glassdoorAPIUtil.js
+++ frontend/util/glassdoorAPIUtil.js
@@ -6,6 +6,6 @@ const key = key;
 export const fetchGlassdoorCompany = company => {
     return axios({
         method: 'GET',
-        url: `http://cors.io/?http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${id}&t.k=${key}&action=employers&q=${company}&userip=192.168.43.42&useragent=Mozilla/%2F4.0`
+        url: `https://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${id}&t.k=${key}&action=employers&q=${company}&userip=192.168.43.42&useragent=Mozilla/%2F4.0`
     });
 };
