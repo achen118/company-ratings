2e2dc8226f604a47b42087d54ca2f88f21ff6656
Alice Chen <alicechen118@gmail.com>
2017-10-12 01:39:54 -0700

    Fix glassdoor api url

--- frontend/util/companiesAPIUtil.js
+++ frontend/util/companiesAPIUtil.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 export const fetchAllCompanies = () => {
     return axios({
         method: 'GET',
-        url: 'https://company-ratings.herokuapp.com/api/companies'
+        url: '/api/companies'
     });
 };
 
 export const addNewCompany = company => {
     return axios({
         method: 'POST',
-        url: 'https://company-ratings.herokuapp.com/api/companies',
+        url: '/api/companies',
         data: { company }
     });
 };
@@ -18,7 +18,7 @@ export const addNewCompany = company => {
 export const updateCompany = company => {
     return axios({
         method: 'PATCH',
-        url: `https://company-ratings.herokuapp.com/api/companies/${company.id}`,
+        url: `/api/companies/${company.id}`,
         data: { company }
     });
 };
