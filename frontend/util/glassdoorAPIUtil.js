7ae1e593e518eb8357a74d689879c422ca24874f
Alice Chen <alicechen118@gmail.com>
2017-10-12 01:34:08 -0700

    Hide api keys

--- frontend/util/glassdoorAPIUtil.js
+++ frontend/util/glassdoorAPIUtil.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { id, key } from './apiKeys';
+
+const id = id;
+const key = key;
 
 export const fetchGlassdoorCompany = company => {
     return axios({
         method: 'GET',
-        url: `http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${id}&t.k=${key}&action=employers&q=${company}&userip=192.168.43.42&useragent=Mozilla/%2F4.0`
+        url: `http://cors.io/?http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=${id}&t.k=${key}&action=employers&q=${company}&userip=192.168.43.42&useragent=Mozilla/%2F4.0`
     });
 };
